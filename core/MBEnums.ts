cefccc7301e8583911703997ad9731dac303cb90
Jin JianXin <9963715@qq.com>
2026-08-06 10:40:15 +0800

    feat(core): add MBEnums

---
+++ core/MBEnums.ts
@@ -0,0 +1,27 @@
+namespace MBEnums {
+
+    /**
+     * 舵机接口
+     */
+    export enum ServoPort {
+        S1,
+        S2
+    }
+
+    /**
+     * 电机接口
+     */
+    export enum MotorPort {
+        M1,
+        M2
+    }
+
+    /**
+     * 电机方向
+     */
+    export enum Direction {
+        Forward,
+        Backward
+    }
+
+}
